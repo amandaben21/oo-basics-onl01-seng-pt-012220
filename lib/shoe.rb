1ba1b5f07f7ca717ee3db8b69c8dd2596dda5f0c
Amanda Contreras <amandaben21@aol.com>
2020-02-09 23:20:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+attr_accessor :brand, :color, :size, :material, :condition
+
+def cobble
+  shoe.condition = "new"
+puts "the shoe has been repaired"
+ end
+end 
+
+ 
+ 
+  
+ 
